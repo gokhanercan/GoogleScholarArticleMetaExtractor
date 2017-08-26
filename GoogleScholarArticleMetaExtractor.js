2e9e8ebe7d73d5a5ea55a95a0e2dfe84bc686371
Gökhan Ercan <gokhan@ercannet.com>
2017-08-26 12:55:07 +0300

    filename format updated

--- GoogleScholarArticleMetaExtractor.js
+++ GoogleScholarArticleMetaExtractor.js
@@ -35,22 +35,21 @@ $("h3[class='gs_rt'] a").mouseover(function(){
   var firstAuthorLastName = firstAuthor.substring(i).trim();
   var year = author.replace( /^\D+/g, '');
   year = parseInt(year);
-  //console.log(firstAuthorLastName);
-  //console.log(cites);
-  //console.log(year);
-  var filename = title+"-"+cites+"c-"+firstAuthorLastName+year+".pdf";
+  
+  //year format
+  var yearStr = year.toString().substr(2,2);
+ 
+  //final filename
+  var filename = firstAuthorLastName.trim() + yearStr.trim() + "-" + title.trim() + " - " + cites.trim() +"c"+".pdf";
+  
   console.log(filename);
   
   //append and copy to clipboard TODO: Bunu otomatik yapamadým bi türlü.
   article.append("<textarea onClick='javascript:copyToClipboard(this.value);' class='article' rows='1' cols='200'>" + filename+"</textarea>");
   var txt = article.parent().find("textarea");
   txt.select();
-  //txt.setSelectionRange(0,100);
-  //txt[0].select();
   document.execCommand('copy');
-  //var copied = txt.innerText.createTextRange();
-  //copied.execCommand('Copy');
-  //copyToClipboard(filename);
-  //window.prompt("Copy to clipboard: Ctrl+C, Enter","gokhan ercan");
+ 
 }); 
+
 
